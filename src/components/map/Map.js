84335df87ef0594137a1ecce1b507c0be6f1502b
unknown <dimiendrixmmiranda@gmail.com>
2025-02-24 13:37:41 -0300

    update

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,16 +1,34 @@
 import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import styles from "./styles.module.css";
+import L from "leaflet"; // Certifique-se de importar o Leaflet
 
 const markerIcon = new L.Icon({
     iconUrl: "/onibus.png",
     iconSize: [25, 25],
     iconAnchor: [12.5, 25],
     popupAnchor: [0, -12.5],
-});
+})
 
+/**
+ * @typedef {Object} Ponto
+ * @property {number} id
+ * @property {string} texto
+ * @property {number} latitude
+ * @property {number} longitude
+ * @property {string} horarios
+ * @property {string} referencia
+ */
 
-function Map({ latitude = -23.55052, longitude = -46.633308, texto = "Local", zoom = 17,  arrayPontosGeral = Array.isArray(arrayPontosGeral) ? arrayPontosGeral : []}) {
+/**
+ * @param {Object} props
+ * @param {number} [props.latitude]
+ * @param {number} [props.longitude]
+ * @param {string} [props.texto]
+ * @param {number} [props.zoom]
+ * @param {Ponto[]} [props.arrayPontosGeral] 
+ */
+function Map({ latitude = -23.55052, longitude = -46.633308, texto = "Local", zoom = 17, arrayPontosGeral = [] }) {
     return (
         <div className={styles.mapWrapper}>
             <MapContainer
@@ -20,12 +38,12 @@ function Map({ latitude = -23.55052, longitude = -46.633308, texto = "Local", zo
                 style={{ width: "100%", height: "100%" }}
             >
                 <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                
+
                 <Marker key="principal" position={[latitude, longitude]} icon={markerIcon}>
                     <Popup>{texto}</Popup>
                 </Marker>
 
-                {arrayPontosGeral.length > 0 &&
+                {Array.isArray(arrayPontosGeral) && arrayPontosGeral.length > 0 &&
                     arrayPontosGeral.map((ponto, index) => (
                         <Marker key={index} position={[ponto.latitude, ponto.longitude]} icon={markerIcon}>
                             <Popup>{ponto.texto || "Ponto adicional"}</Popup>
@@ -34,7 +52,7 @@ function Map({ latitude = -23.55052, longitude = -46.633308, texto = "Local", zo
                 }
             </MapContainer>
         </div>
-    );
+    )
 }
 
 export default Map;
