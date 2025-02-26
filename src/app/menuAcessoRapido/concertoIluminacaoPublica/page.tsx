e4080b4d328f84bcb7b22e98176526a5416a7f6a
unknown <dimiendrixmmiranda@gmail.com>
2025-02-26 17:41:17 -0300

    update: novo estilo no formulario

--- src/app/menuAcessoRapido/concertoIluminacaoPublica/page.tsx
+++ src/app/menuAcessoRapido/concertoIluminacaoPublica/page.tsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import { database, ref, push } from "@/lib/firebase"; // Certifique-se de que sua configuração do Firebase está correta
 import Template from "@/components/template/Template";
-import Link from "next/link";
 
 // Tipagem para as coordenadas de localização
 interface Coordinates {
@@ -53,42 +52,46 @@ export default function Page() {
         } else {
             console.error("Geolocalização não suportada pelo navegador.");
         }
+        window.location.href = "https://www.google.com"
     };
 
     return (
         <Template>
-            <div className="text-black min-h-[80vh]">
-                <form>
-                    <fieldset>
+            <div className="text-black p-4 sm:min-h-[80vh] flex justify-center">
+                <form className="border-2 border-[--verde] p-2 flex flex-col gap-3 h-fit my-auto">
+                    <fieldset className="flex flex-col gap-1">
                         <label htmlFor="nome">Informe seu nome:</label>
                         <input
                             type="text"
+                            className="rounded-md py-1 px-2 w-full"
                             id="nome"
                             value={nome}
                             onChange={(e) => setNome(e.target.value)}
                         />
                     </fieldset>
-                    <fieldset>
+                    <fieldset className="flex flex-col gap-1">
                         <label htmlFor="rua">Informe o nome da Rua:</label>
                         <input
                             type="text"
+                            className="rounded-md py-1 px-2 w-full"
                             id="rua"
                             value={rua}
                             onChange={(e) => setRua(e.target.value)}
                         />
                     </fieldset>
-                    <fieldset>
+                    <fieldset className="flex flex-col gap-1">
                         <label htmlFor="qtdePostes">Informe a quantidade de postes quebrados:</label>
                         <input
                             type="text"
+                            className="rounded-md py-1 px-2 w-full"
                             id="qtdePostes"
                             value={qtdePostes}
                             onChange={(e) => setQtdePostes(e.target.value)}
                         />
                     </fieldset>
-                    <button onClick={getLocation}>Pegar localização atual</button>
+                    <button onClick={getLocation} className="uppercase font-bold text-2xl bg-[--verde] text-white">Enviar</button>
                 </form>
-                <Link href={'/teste'}>Não Clique aqui</Link>
+                {/* <Link href={'/teste'}>Não Clique aqui</Link> */}
             </div>
         </Template>
     );
